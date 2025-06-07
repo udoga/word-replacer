f501a60afdf69304cfe073bff8ba82f74f345bfd
Ugur Doga Sezgin <doga.sezgin@gmail.com>
2025-06-07 14:14:50 +0100

    add: roberta model can find contextualized representations of tokens

--- source/roberta_model.py
+++ source/roberta_model.py
@@ -6,6 +6,7 @@ class RobertaModel:
     def __init__(self):
         self.tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
         self.model = RobertaForMaskedLM.from_pretrained('roberta-base', output_hidden_states=True)
+        self.output = None
 
     def get_vocabulary_size(self):
         return len(self.tokenizer)
@@ -16,11 +17,24 @@ class RobertaModel:
     def get_tokens_from_ids(self, token_ids):
         return [c.strip() for c in self.tokenizer.batch_decode(token_ids)]
 
-    def get_input_embeddings(self, token_ids) -> Tensor:
+    def get_input_embeddings(self, encoding) -> Tensor:
+        return self.get_batch_input_embeddings([encoding])[0]
+
+    def get_batch_input_embeddings(self, encodings) -> Tensor:
         with torch.no_grad():
-            return self.model.get_input_embeddings()(torch.tensor([token_ids]))[0]
+            return self.model.get_input_embeddings()(torch.tensor(encodings))
 
     def get_prediction_logits(self, input_embeddings, target_position) -> Tensor:
         with torch.no_grad():
-            model_output = self.model(inputs_embeds=input_embeddings.unsqueeze(0))
-        return model_output.logits[0][target_position]
+            return self.model(inputs_embeds=input_embeddings.unsqueeze(0)).logits[0][target_position]
+
+    def get_hidden_states(self) -> tuple:
+        return self.output.hidden_states if self.output else None
+
+    def load_encodings(self, encodings):
+        with torch.no_grad():
+            self.output = self.model(torch.tensor(encodings))
+
+    def get_contextualized_representations(self, token_index, layer_count) -> Tensor:
+        layer_indices = [-i for i in range(1, layer_count + 1)]
+        return torch.cat(tuple([self.output.hidden_states[i][:, token_index, :] for i in layer_indices]), dim=1)
--- test/roberta_model_test.py
+++ test/roberta_model_test.py
@@ -1,4 +1,5 @@
 import torch
+from torch import Tensor, testing
 from unittest import TestCase
 from source.roberta_model import RobertaModel
 
@@ -16,13 +17,27 @@ class RobertaModelTest(TestCase):
         self.assertEqual(["hello"], self.model.get_tokens_from_ids([20760]))
         self.assertEqual(["hello", "world"], self.model.get_tokens_from_ids([20760, 232]))
 
-    def test_gets_input_embeddings_from_ids(self):
-        embeddings = self.model.get_input_embeddings([0, 20760, 2])
-        self.assertTrue(torch.is_tensor(embeddings))
+    def test_gets_input_embeddings_from_encoding(self):
+        embeddings: Tensor = self.model.get_input_embeddings([0, 20760, 2])
+        batch_embeddings: Tensor = self.model.get_batch_input_embeddings([[0, 20760, 2], [0, 232, 2]])
         self.assertEqual((3, 768), embeddings.shape)
+        self.assertEqual((2, 3, 768), batch_embeddings.shape)
 
     def test_gets_predictions_logits(self):
-        embeddings = self.model.get_input_embeddings([0, 20760, 2])
-        logits = self.model.get_prediction_logits(embeddings, 1)
-        self.assertTrue(torch.is_tensor(logits))
+        embeddings: Tensor = self.model.get_input_embeddings([0, 20760, 2])
+        logits: Tensor = self.model.get_prediction_logits(embeddings, 1)
         self.assertEqual((self.model.get_vocabulary_size(),), logits.shape)
+
+    def test_loads_encodings(self):
+        self.model.load_encodings([[0, 20760, 2], [0, 232, 2]])
+        hidden_states: tuple = self.model.get_hidden_states()
+        self.assertEqual(13, len(hidden_states))
+        self.assertEqual((2, 3, 768), hidden_states[0].shape)
+
+    def test_finds_contextual_representation(self):
+        encodings = [[0, 20760, 2], [0, 232, 2]]
+        self.model.load_encodings(encodings)
+        states: tuple = self.model.get_hidden_states()
+        representations: Tensor = self.model.get_contextualized_representations(1, 2)
+        testing.assert_close(torch.concat([states[-1][0][1], states[-2][0][1]], dim=0), representations[0])
+        testing.assert_close(torch.concat([states[-1][1][1], states[-2][1][1]], dim=0), representations[1])
