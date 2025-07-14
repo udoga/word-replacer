4fcd1ba40be8f8e06f6a7baaeb6fb21eb3228af3
Ugur Doga Sezgin <doga.sezgin@gmail.com>
2025-07-14 13:13:28 +0100

    fix: failing tests because of argument order

--- test/benchmark_reporter_test.py
+++ test/benchmark_reporter_test.py
@@ -36,12 +36,12 @@ class BenchmarkReporterTest(TestCase):
         self.assertEqual(3, df.loc[1]["substitutes"]["intelligent"])
 
     def test_loads_desired_number_of_rows(self):
-        self.reporter.load_dataset("lst_trial", 1)
+        self.reporter.load_dataset("lst_trial", row_count=1)
         df = self.reporter.get_frame()
         self.assertEqual(1, len(df))
 
     def test_loads_predictions(self):
-        self.reporter.load_dataset("lst_trial", 3)
+        self.reporter.load_dataset("lst_trial", row_count=3)
         self.substituter.load_responses([["intelligent"], ["luminous"], ["brilliant"]])
         self.reporter.load_predictions(self.substituter)
         predictions_column = self.reporter.get_frame()["predictions"].to_list()
@@ -50,7 +50,7 @@ class BenchmarkReporterTest(TestCase):
         self.assertEqual(["brilliant"], predictions_column[2])
 
     def test_calculates_best_scores(self):
-        self.reporter.load_dataset("lst_trial", 3)
+        self.reporter.load_dataset("lst_trial", row_count=3)
         self.substituter.load_responses([["intelligent"], ["luminous"], ["brilliant"]])
         self.reporter.load_predictions(self.substituter)
         self.reporter.load_scores()
@@ -58,7 +58,7 @@ class BenchmarkReporterTest(TestCase):
         self.assertAlmostEqual(mean([3/7, 2/5, 1/5]), self.reporter.get_average_scores()["best_score"], places=6)
 
     def test_calculates_best_mode_scores(self):
-        self.reporter.load_dataset("lst_trial", 3)
+        self.reporter.load_dataset("lst_trial", row_count=3)
         self.substituter.load_responses([["clever"], ["luminous"], ["brilliant"]])
         self.reporter.load_predictions(self.substituter)
         self.reporter.load_scores()
@@ -66,7 +66,7 @@ class BenchmarkReporterTest(TestCase):
         self.assertAlmostEqual(mean([1, 1, 0][1:]), self.reporter.get_average_scores()["best_mode_score"], places=6)
 
     def test_calculates_oot_scores(self):
-        self.reporter.load_dataset("lst_trial", 3)
+        self.reporter.load_dataset("lst_trial", row_count=3)
         self.substituter.load_responses([["x", "smart"], ["x", "x"], ["gleam", "colourful"]])
         self.reporter.load_predictions(self.substituter)
         self.reporter.load_scores()
@@ -74,7 +74,7 @@ class BenchmarkReporterTest(TestCase):
         self.assertAlmostEqual(mean([1/7, 0, 1/5 + 2/5]), self.reporter.get_average_scores()["oot_score"], places=6)
 
     def test_calculates_oot_mode_scores(self):
-        self.reporter.load_dataset("lst_trial", 3)
+        self.reporter.load_dataset("lst_trial", row_count=3)
         self.substituter.load_responses([["x", "clever"], ["x", "luminous"], ["gleam", "x"]])
         self.reporter.load_predictions(self.substituter)
         self.reporter.load_scores()
@@ -82,11 +82,11 @@ class BenchmarkReporterTest(TestCase):
         self.assertAlmostEqual(mean([1, 1, 0][1:]), self.reporter.get_average_scores()["oot_mode_score"], places=6)
 
     def test_finds_if_there_is_tie_in_most_voted_substitutes(self):
-        self.reporter.load_dataset("lst_trial", 3)
+        self.reporter.load_dataset("lst_trial", row_count=3)
         self.assertEqual([True, False, False], self.reporter.get_frame()["tie"].to_list())
 
     def test_skips_instances_with_missing_predictions(self):
-        self.reporter.load_dataset("lst_trial", 3)
+        self.reporter.load_dataset("lst_trial", row_count=3)
         self.substituter.load_responses([["intelligent"], [], ["colourful"]])
         self.reporter.load_predictions(self.substituter)
         self.reporter.load_scores()
@@ -100,7 +100,7 @@ class BenchmarkReporterTest(TestCase):
         self.assertAlmostEqual(mean([1]), self.reporter.get_average_scores()["oot_mode_score"], places=6)
 
     def test_counts_prediction_as_correct_when_it_has_different_form(self):
-        self.reporter.load_dataset("lst_trial", 3)
+        self.reporter.load_dataset("lst_trial", row_count=3)
         self.substituter.load_responses([["smartest"], ["clearer"], ["gleaming"]])
         self.reporter.load_predictions(self.substituter)
         self.reporter.load_scores()
