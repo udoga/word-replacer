0f1c024227efe002782224b780421a813851e4da
Ugur Doga Sezgin <doga.sezgin@gmail.com>
2025-07-30 17:20:31 +0100

    add: library versions in requirements.txt

--- setup_linux.sh
+++ setup_linux.sh
@@ -7,7 +7,7 @@ sudo apt update -y
 sudo apt install -y python"${PY_VERSION}"-venv
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 python -m nltk.downloader wordnet stopwords
 ./download_dataset.sh
 pytest
